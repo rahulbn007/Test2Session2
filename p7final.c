0b896372e806286260e0fc5eae930c0f7e4248e0
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-02-24 10:53:29 +0000

    .

--- p7final.c
+++ p7final.c
@@ -1,1 +1,41 @@
-
+#include<stdio.h>
+#include<math.h>
+typedef struct _point
+{
+  float x,y;
+}Point;
+typedef struct _line
+{
+  Point p1,p2;
+  float distance;
+}Line;
+Point input_point()
+{
+  Point t;
+  printf("Enter the coordinates\n");
+  scanf("%f%f",&t.x,&t.y);
+  return t;
+}
+Line input_line()
+{
+  Line t;
+  t.p1=input_point();
+  t.p2=input_point();
+  return t;
+}
+void find_length(Line *l)
+{
+  l->distance=sqrt(((l->p2.x-l->p1.x)*(l->p2.x-l->p1.x))+((l->p2.y-l->p1.y)*(l->p2.y-l->p1.y)));
+}
+void output(Line l)
+{
+  printf("The length of the line with coordinates\n%f,%f\n%f,%f\nis %f",l.p1.x,l.p1.y,l.p2.x,l.p2.y,l.distance);
+}
+int main()
+{
+  Line l;
+  l=input_line();
+  find_length(&l);
+  output(l);
+  return 0;
+}
